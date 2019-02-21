260a021249c3f307d1b63f9b8c8a9d8934ac0130
Nguyễn Tùng Lâm <ntlisme@hotmail.com>
2019-02-21 17:02:39 +0700

    making changes

---
+++ src/app/product.model.ts
@@ -0,0 +1,12 @@
+/**
+ * Provides product object
+ */
+export class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number
+  ) {}
+}
